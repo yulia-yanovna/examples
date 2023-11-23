ad04214a696b618f0bb595e13c227734a05f18fe
Yulia Gordienko <julia.yanovna@gmail.com>
2023-11-23 21:03:31 +0300

    Создали команды

---
+++ example002_HelloUser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+static string ReadLineUTF()
+{
+    ConsoleKeyInfo currentKey;
+    var sBuilder = new StringBuilder();
+    do
+    {
+        currentKey = Console.ReadKey();
+        if (currentKey.Key != ConsoleKey.Enter)
+            sBuilder.Append(currentKey.KeyChar);
+    }
+    while (currentKey.Key != ConsoleKey.Enter);
+    Console.WriteLine();
+    return sBuilder.ToString();
+}
+Console.Write("ВВедите ваше имя ");
+string username = ReadLineUTF();
+Console.Write("Привет, ");
+Console.Write(username);
+
+
+
