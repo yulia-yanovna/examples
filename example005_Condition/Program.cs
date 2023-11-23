7c6b2b414cf6419fd7fe1d554f1bd87287b570b8
Yulia Gordienko <julia.yanovna@gmail.com>
2023-11-23 22:29:43 +0300

    Добавили example005 c именами

---
+++ example005_Condition/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+static string ReadLineUTF()
+{
+    ConsoleKeyInfo currentKey;
+    var sBuilder = new StringBuilder();
+    do
+    {
+        currentKey = Console.ReadKey();
+        if (currentKey.Key != ConsoleKey.Enter)
+            sBuilder.Append(currentKey.KeyChar);
+    }
+    while (currentKey.Key != ConsoleKey.Enter);
+    Console.WriteLine();
+    return sBuilder.ToString();
+}
+
+
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "yulia")
+{
+    Console.WriteLine("Ура, это же Yulia");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username);
+}
